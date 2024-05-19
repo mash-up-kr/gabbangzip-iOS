be9b8ab6b52f30428edc9e74ed00230ce5c2013c
Serena <sha0720@naver.com>
2024-05-19 15:28:49 +0900

    chore: tuist 적용 및 프로젝트 파일 생성
    (#4)

---
+++ MyApp/Project.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+
+let project = Project(
+    name: "MyApp",
+    targets: [
+        .target(
+            name: "MyApp",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.MyApp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["MyApp/Sources/**"],
+            resources: ["MyApp/Resources/**"],
+            dependencies: [
+                .external(name: "TCA"),
+                .external(name: "Nuke"),
+                .external(name: "Lottie"),
+                .external(name: "Get")
+            ]
+        ),
+        .target(
+            name: "MyAppTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.MyAppTests",
+            infoPlist: .default,
+            sources: ["MyApp/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "MyApp")]
+        ),
+    ]
+)
---
+++ MyApp/Tuist/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+#if TUIST
+    import ProjectDescription
+
+    let packageSettings = PackageSettings(
+        productTypes: [
+            "TCA": .framework,
+            "Nuke": .framwork,
+            "Lottie": .framework,
+            "Get": .framework
+        ]
+    )
+#endif
+
+let package = Package(
+    name: "MyApp",
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.10.0"),
+        .package(url: "https://github.com/kean/Nuke.git", from: "12.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.3"),
+        .package(url: "https://github.com/kean/Get.git", from: "2.2")
+    ]
+)
