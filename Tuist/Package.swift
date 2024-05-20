6e9eef1ceceec051551cd3941acba536e2c88289
Serena <sha0720@naver.com>
2024-05-20 11:21:22 +0900

    chore: Package Nuke, Get Version 수정

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,8 +18,8 @@ let package = Package(
   name: "MyApp",
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.10.0"),
-    .package(url: "https://github.com/kean/Nuke.git", from: "12.0"),
+    .package(url: "https://github.com/kean/Nuke.git", from: "12.0.0"),
     .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.4.3"),
-    .package(url: "https://github.com/kean/Get.git", from: "2.2")
+    .package(url: "https://github.com/kean/Get.git", from: "2.2.0")
   ]
 )
