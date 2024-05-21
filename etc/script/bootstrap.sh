41d114e4c0392eb63297e639ab037d6a52151fa1
Chanwoo Cho <72292617+GREENOVER@users.noreply.github.com>
2024-05-21 11:20:57 +0900

    infra: bootstrap 줄바꿈 및 불필요 명령 제거
    Co-authored-by: JoonHyeok Yang <zzing1031@gmail.com>

--- etc/script/bootstrap.sh
+++ etc/script/bootstrap.sh
@@ -21,7 +21,7 @@ echo "- Project Directory: ${PROJECT_DIR}"
 echo "------------------------------------------------------------------"
 
 # Homebrew 설치 및 업데이트
-echo "\n[0] > Installing or Updating Homebrew ...\n"
+echo -e "\n[0] > Installing or Updating Homebrew ...\n"
 if ! command -v brew &> /dev/null; then
   echo "Homebrew is not installed. Installing Homebrew ..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -31,7 +31,7 @@ else
 fi
 
 # Ruby 버전 고정 및 설치
-echo "\n[1] > Installing Ruby version from .ruby-version ...\n"
+echo -e "\n[1] > Installing Ruby version from .ruby-version ...\n"
 if ! command -v rbenv &> /dev/null; then
   echo "rbenv is not installed. Installing rbenv ..."
   brew install rbenv
@@ -51,25 +51,21 @@ rbenv global $RUBY_VERSION
 echo "Ruby version: $(ruby -v)"
 
 # Ruby Bundler 설치
-echo "\n[2] > Installing Ruby Bundler ...\n"
+echo -e "\n[2] > Installing Ruby Bundler ...\n"
 gem install bundler
 
 # Bundle 업데이트(Fastlane 설치)
-echo "\n[3] > Updating Bundle (with Fastlane) ...\n"
+echo -e "\n[3] > Updating Bundle (with Fastlane) ...\n"
 BUNDLE_GEMFILE="${PROJECT_DIR}/Gemfile" bundle update
 
-# tuistenv tuist 제거
-echo "\n[4] > Uninstalling Tuist (tuistenv) ...\n"
-curl -Ls https://uninstall.tuist.io | bash
-
 # mise 설치
-echo "\n[5] > Installing mise ...\n"
+echo -e "\n[5] > Installing mise ...\n"
 curl https://mise.run | sh
 
 # mise 활성화
-echo "\n[6] > Activating mise ...\n"
+echo -e "\n[6] > Activating mise ...\n"
 echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 
-echo "\n---------------------------------"
+echo -e "\n---------------------------------"
 echo "::: Bootstrap Script Finished :::"
-echo "---------------------------------\n"
+echo -e "---------------------------------\n"
