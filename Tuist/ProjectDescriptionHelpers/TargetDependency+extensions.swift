514fd6f2b60c6c97d6c24b4266c1893093b7f046
YangJoonHyeok <zzing1031@gmail.com>
2024-05-26 23:29:17 +0900

    feat: 타겟 및 경로 관리를 위한 타입 생성

---
+++ Tuist/ProjectDescriptionHelpers/TargetDependency+extensions.swift
@@ -0,0 +1,51 @@
+//
+//  TargetDependency+extensions.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by YangJoonHyeok on 5/26/24.
+//
+
+import Foundation
+import ProjectDescription
+
+extension TargetDependency {
+	public static func external(externalDependency: ExternalDependency) -> TargetDependency {
+		return .external(name: externalDependency.rawValue)
+	}
+	
+	public static func target(name: TargetName) -> TargetDependency {
+		return .target(name: name.rawValue)
+	}
+	
+	public static func project(target: TargetName, projectPath: ProjectPath) -> TargetDependency {
+		return .project(
+			target: target.rawValue,
+			path: .relativeToRoot(projectPath.rawValue)
+		)
+	}
+}
+
+public enum ProjectPath: String {
+	case core = "Projects/Core"
+	case designSystem = "Projects/DesignSystem"
+	case coordinator = "Projects/Features/Coordinator"
+	case scene = "Projects/Features/Scene"
+}
+
+public enum TargetName: String {
+	case models = "Models"
+	case getHelpers = "GetHelpers"
+	case gabbangzipError = "GabbangzipError"
+	case coreKit = "CoreKit"
+	case designSystem = "DesignSystem"
+	case appCoordinator = "AppCoordinator"
+	case mainCoordinator = "MainCoordinator"
+}
+
+public enum ExternalDependency: String {
+	case get = "Get"
+	case composableArchitecture = "ComposableArchitecture"
+	case tcaCoordinators = "TCACoordinators"
+	case nuke = "Nuke"
+	case lottie = "Lottie"
+}
