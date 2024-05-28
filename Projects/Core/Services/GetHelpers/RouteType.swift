8cd05a21efc5ea25237e3887d19619946d1ef758
YangJoonHyeok <zzing1031@gmail.com>
2024-05-28 09:15:37 +0900

    chore: 불필요한 코드 삭제

--- Projects/Core/Services/GetHelpers/RouteType.swift
+++ Projects/Core/Services/GetHelpers/RouteType.swift
@@ -10,9 +10,6 @@ import Foundation
 import Get
 
 public protocol RouteType {
-  /// The target's base `URL`.
-  var baseURL: URL { get }
-
   /// The path to be appended to `baseURL` to form the full `URL`.
   var path: String { get }
   
