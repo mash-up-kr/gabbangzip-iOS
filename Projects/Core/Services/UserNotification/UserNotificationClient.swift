645005e4a9dccceda15fec07cc7d67c1543eb42f
YangJoonHyeok <zzing1031@gmail.com>
2024-06-08 13:49:08 +0900

    fix: 줄바꿈 처리

--- Projects/Core/Services/UserNotification/UserNotificationClient.swift
+++ Projects/Core/Services/UserNotification/UserNotificationClient.swift
@@ -15,8 +15,16 @@ public struct UserNotificationClient {
   public var requestAuthorization: @Sendable () -> Void
   
   public enum DelegateEvent {
-    case didReceiveResponse(UNNotificationResponse, completionHandler: @Sendable () -> Void)
-    case willPresentNotification(UNNotification, completionHandler: @Sendable (UNNotificationPresentationOptions)  -> Void)
+    case didReceiveResponse(
+      UNNotificationResponse,
+      completionHandler: @Sendable () -> Void
+    )
+    case willPresentNotification(
+      UNNotification,
+      completionHandler: @Sendable (
+        UNNotificationPresentationOptions
+      ) -> Void
+    )
   }
 }
 
@@ -57,18 +65,28 @@ extension UserNotificationClient: DependencyKey {
 }
 
 extension UserNotificationClient {
-  fileprivate class Delegate: NSObject,  UNUserNotificationCenterDelegate {
+  fileprivate class Delegate: NSObject, UNUserNotificationCenterDelegate {
     let continuation: AsyncStream<DelegateEvent>.Continuation
     
     init(continuation: AsyncStream<DelegateEvent>.Continuation) {
       self.continuation = continuation
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    func userNotificationCenter(
+      _ center: UNUserNotificationCenter,
+      willPresent notification: UNNotification,
+      withCompletionHandler completionHandler: @escaping (
+        UNNotificationPresentationOptions
+      ) -> Void
+    ) {
       continuation.yield(.willPresentNotification(notification, completionHandler: { completionHandler($0) }))
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    func userNotificationCenter(
+      _ center: UNUserNotificationCenter,
+      didReceive response: UNNotificationResponse,
+      withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
       continuation.yield(.didReceiveResponse(response, completionHandler: { completionHandler() }))
     }
   }
