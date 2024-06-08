a03f872372655b7c94b069d863d2dd1a4bee4b5b
YangJoonHyeok <zzing1031@gmail.com>
2024-06-08 12:27:17 +0900

    feat: AppDelegate 추가

---
+++ Projects/App/Sources/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  App
+//
+//  Created by YangJoonHyeok on 6/8/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+import ComposableArchitecture
+import UIKit
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    return true
+  }
+}
