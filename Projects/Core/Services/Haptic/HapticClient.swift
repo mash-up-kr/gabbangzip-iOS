345c54da51c472c8e0bab43a6142d075f03dfebf
Chanwoo Cho <72292617+GREENOVER@users.noreply.github.com>
2024-06-10 10:51:23 +0900

    [Feat] HapticClient 구현 (#32)
    * feat: HapticClient 구현
    
    * typo: 린트 주석 설명 변경
    
    * fix: 불필요 공백 제거
    
    * refactor: 리뷰 반영
    
    Co-authored-by: JoonHyeok Yang <zzing1031@gmail.com>
    
    ---------
    
    Co-authored-by: JoonHyeok Yang <zzing1031@gmail.com>

---
+++ Projects/Core/Services/Haptic/HapticClient.swift
@@ -0,0 +1,39 @@
+//
+//  HapticClient.swift
+//  Services
+//
+//  Created by GREEN on 6/8/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - API Client Interface
+public struct HapticClient {
+  /// 사용자 인터랙션에 따른 햅틱 반응 (light || medium || heavy || soft || rigid)
+  public var triggerImpact: @Sendable (UIImpactFeedbackGenerator.FeedbackStyle) async -> Void
+  /// 작업 완료 여부에 따른 햅틱 반응 (success || warning || error)
+  public var triggerNotification: @Sendable (UINotificationFeedbackGenerator.FeedbackType) async -> Void
+}
+
+public extension DependencyValues {
+  var hapticClient: HapticClient {
+    get { self[HapticClient.self] }
+    set { self[HapticClient.self] = newValue }
+  }
+}
+
+// MARK: - API Client Implementation
+extension HapticClient: DependencyKey {
+  public static let liveValue = HapticClient(
+    triggerImpact: { style in
+      let generator = await UIImpactFeedbackGenerator(style: style)
+      await generator.impactOccurred()
+    },
+    triggerNotification: { type in
+      let generator = await UINotificationFeedbackGenerator()
+      await generator.notificationOccurred(type)
+    }
+  )
+}
