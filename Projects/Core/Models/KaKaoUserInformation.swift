61991112fc9fad30db4dd2ec6b2dd5ea9733d8b0
Serena <sha0720@naver.com>
2024-06-19 19:52:57 +0900

    feat: KakaoUserInformation DTO 생성

---
+++ Projects/Core/Models/KaKaoUserInformation.swift
@@ -0,0 +1,15 @@
+//
+//  KaKaoUserInformation.swift
+//  Models
+//
+//  Created by Hyun A Song on 6/19/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+import Foundation
+
+public struct KaKaoUserInformation: Codable {
+  public let idToken: String?
+  public let nickName: String?
+  public let profileImageUrl: URL?
+}
