e589df155c9b71f001b5a711560c223f25332e3b
hyerin <hope1053@naver.com>
2024-06-20 14:24:04 +0900

    [Chore] TCA 템플릿 보완 (#55)
    * chore: Main target 타입 framework로 변경
    
    * chore: Preview 코드 추가
    
    * chore: 서픽스 Core 추가
    
    * fix: staticFramework로 변경

--- Xcode Templates/Feature.xctemplate/___FILEBASENAME___View.swift
+++ Xcode Templates/Feature.xctemplate/___FILEBASENAME___View.swift
@@ -4,13 +4,21 @@ import ComposableArchitecture
 import SwiftUI
 
 public struct ___FILEBASENAMEASIDENTIFIER___: View {
-  let store: StoreOf<___VARIABLE_productName:identifier___>
+  let store: StoreOf<___VARIABLE_productName:identifier___Core>
 
-  public init(store: StoreOf<___VARIABLE_productName:identifier___>) {
+  public init(store: StoreOf<___VARIABLE_productName:identifier___Core>) {
     self.store = store
   }
 
   public var body: some View {
     Text("Hello, World!")
   }
+}
+
+#Preview {
+  ___FILEBASENAMEASIDENTIFIER___(
+    store: Store(initialState: .init()) {
+      ___VARIABLE_productName:identifier___Core()
+    }
+  )
 }
