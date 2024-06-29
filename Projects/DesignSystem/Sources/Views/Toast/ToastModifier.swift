feaafbc97f6d36778e3185d54550291236c30d93
chanwoo <humains29@gmail.com>
2024-06-29 22:34:30 +0900

    design: 토스트 2초 조건 설정

--- Projects/DesignSystem/Sources/Views/Toast/ToastModifier.swift
+++ Projects/DesignSystem/Sources/Views/Toast/ToastModifier.swift
@@ -11,6 +11,7 @@ import SwiftUI
 public struct ToastModifier: ViewModifier {
   @Binding var isPresented: Bool
   let type: ToastType
+  let time: CGFloat
   
   public func body(content: Content) -> some View {
     if isPresented {
@@ -20,10 +21,19 @@ public struct ToastModifier: ViewModifier {
             VStack(spacing: 0) {
               ToastView(type: type)
                 .padding(.top, 15)
+                .transition(.opacity)
                 .animation(.spring(), value: isPresented)
               
               Spacer()
             }
+            .onAppear {
+              DispatchQueue.main.asyncAfter(deadline: .now() + time) {
+                withAnimation {
+                  isPresented = false
+                }
+              }
+            }
+            .opacity(isPresented ? 1 : 0)
           }
         )
     }
@@ -33,12 +43,14 @@ public struct ToastModifier: ViewModifier {
 public extension View {
   func toast(
     isPresented: Binding<Bool>,
-    type: ToastType
+    type: ToastType,
+    time: CGFloat = 2.0
   ) -> some View {
     self.modifier(
       ToastModifier(
         isPresented: isPresented,
-        type: type
+        type: type,
+        time: time
       )
     )
   }
