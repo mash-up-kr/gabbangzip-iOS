432b8ad1ae53cffe25e2b6f327ffa99c378410c5
Serena <sha0720@naver.com>
2024-07-02 20:57:50 +0900

    refactor: KakaoLogin 디펜던시를 갖는 객체 logger를 print문으로 대체

--- Projects/Features/Scene/KakaoLoginScene/KakaoLogin/LoginCore.swift
+++ Projects/Features/Scene/KakaoLoginScene/KakaoLogin/LoginCore.swift
@@ -155,7 +155,7 @@ public struct LoginCore {
         
       case let .loginResponse(.failure(error)):
         return .run { send in
-          logger.error("Fail to Login \(error)")
+          print("Fail to Login \(error)")
           await send(.showError(true))
         }
         
@@ -166,7 +166,7 @@ public struct LoginCore {
         }
         
       case let .saveTokenInKeyChain(.failure(error)):
-        logger.error("Fail to save Token in KeyChain \(error)")
+        print("Fail to save Token in KeyChain \(error)")
         return .none
         
       case let .showError(isPresented):
