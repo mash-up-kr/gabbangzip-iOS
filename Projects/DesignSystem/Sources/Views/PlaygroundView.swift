73f58dbc52f90f954bbc3729dac7f796da0cb621
YangJoonHyeok <zzing1031@gmail.com>
2024-07-05 21:19:10 +0900

    chore: PlaygroundView 추가

---
+++ Projects/DesignSystem/Sources/Views/PlaygroundView.swift
@@ -0,0 +1,19 @@
+//
+//  PlaygroundView.swift
+//  DesignSystem
+//
+//  Created by YangJoonHyeok on 7/1/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlaygroundView: View {
+  var body: some View {
+    Text("Hello, world!")
+  }
+}
+
+#Preview {
+  PlaygroundView()
+}
