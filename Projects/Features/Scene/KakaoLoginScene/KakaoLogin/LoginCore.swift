1acbee4e7bffdd320cffac2e62f9e151190d97fb
Serena <sha0720@naver.com>
2024-07-05 15:40:26 +0900

    chore: 프로퍼티 순서 변경

--- Projects/Features/Scene/KakaoLoginScene/KakaoLogin/LoginCore.swift
+++ Projects/Features/Scene/KakaoLoginScene/KakaoLogin/LoginCore.swift
@@ -19,11 +19,11 @@ public struct LoginCore {
   
   @ObservableState
   public struct State: Equatable {
+    public let loginTitle = "우리가 픽! 하는\n우리끼리 네컷앨범"
+    public let loginErrorMessage = "로그인에 실패했어요."
     public var isPresented: Bool
     public var kakaoUser: KaKaoUserInfo
     public var kakaoIdToken: KakaoToken
-    public let loginTitle = "우리가 픽! 하는\n우리끼리 네컷앨범"
-    public let loginErrorMessage = "로그인에 실패했어요."
     
     public init(
       isPresented: Bool = false,
