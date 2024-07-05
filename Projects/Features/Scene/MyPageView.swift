29b11ba28ee6b7a5a2da87639481b4313023142f
Serena <sha0720@naver.com>
2024-07-05 16:33:31 +0900

    feat: MyPageView 그리기

--- Projects/Features/Scene/MyPageView.swift
+++ Projects/Features/Scene/MyPageView.swift
@@ -18,16 +18,102 @@ public struct MyPageView: View {
   }
   
   public var body: some View {
-    NavigationBar(type: .titleWithBackButton(store.myPageTitle))
-    
-    
+    VStack {
+      NavigationBar(type: .titleWithBackButton(store.myPageTitle))
+      
+      Text(store.nickname)
+        .font(.head20)
+        .foregroundColor(DesignSystem.Colors.gray80)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.all, 16)
+      
+      Spacer()
+        .frame(height: 16)
+      
+      Color(DesignSystem.Colors.gray20)
+        .frame(height: 16)
+      
+      Spacer()
+        .frame(height: 16)
+      
+      Text(store.alarmSetting)
+        .font(.head14)
+        .foregroundColor(DesignSystem.Colors.gray80)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, 20)
+        .padding(.horizontal, 16)
+      
+      HStack {
+        Text(store.appAlarm)
+          .font(.body16)
+          .foregroundColor(DesignSystem.Colors.gray80)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.vertical, 20)
+          .padding(.horizontal, 16)
+        
+        Text(store.alarmStatus)
+          .font(.body16)
+          .foregroundColor(DesignSystem.Colors.gray60)
+          .frame(maxWidth: .infinity, alignment: .trailing)
+          .padding(.vertical, 20)
+          .padding(.horizontal, 16)
+      }
+      
+      Spacer()
+        .frame(height: 10)
+      
+      Color(DesignSystem.Colors.gray20)
+        .frame(height: 2)
+      
+      Spacer()
+        .frame(height: 10)
+      
+      Text(store.userSetting)
+        .font(.head14)
+        .foregroundColor(DesignSystem.Colors.gray80)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, 20)
+        .padding(.horizontal, 16)
+      
+      HStack {
+        Text(store.version)
+          .font(.body16)
+          .foregroundColor(DesignSystem.Colors.gray80)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.vertical, 20)
+          .padding(.horizontal, 16)
+        
+        Text(store.currentVersion)
+          .font(.body16)
+          .foregroundColor(DesignSystem.Colors.gray60)
+          .frame(maxWidth: .infinity, alignment: .trailing)
+          .padding(.vertical, 20)
+          .padding(.horizontal, 16)
+      }
+      
+      Text(store.logout)
+        .font(.body16)
+        .foregroundColor(DesignSystem.Colors.gray80)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, 20)
+        .padding(.horizontal, 16)
+      
+      Text(store.unregister)
+        .font(.body16)
+        .foregroundColor(DesignSystem.Colors.gray80)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, 20)
+        .padding(.horizontal, 16)
+      
+      Spacer()
+    }
   }
 }
 
 #Preview {
   MyPageView(
     store: Store(
-      initialState: .init(nickname: "가빵집"),
+      initialState: MyPageCore.State(alarmStatus: "on", nickname: "가빵집"),
       reducer: MyPageCore.init
     )
   )
