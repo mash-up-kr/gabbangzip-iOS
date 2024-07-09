8194728dfaa7b2a19cad9d5ee3651a3431b4cc0a
YangJoonHyeok <zzing1031@gmail.com>
2024-07-09 23:34:22 +0900

    chore: 템플릿에서 Reducer안에 Action 주석 추가

--- Xcode Templates/Reducer.xctemplate/___FILEBASENAME___Core.swift
+++ Xcode Templates/Reducer.xctemplate/___FILEBASENAME___Core.swift
@@ -11,6 +11,9 @@ public struct ___VARIABLE_productName:identifier___Core {
   }
 
   public enum Action {
+    // View Action
+    // Internal Action
+    // Route Action
   }
 
   public var body: some Reducer<State, Action> {
