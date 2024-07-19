1e3e32792ebc99f22caef7db1ff4e27376018550
Serena <sha0720@naver.com>
2024-07-19 19:51:46 +0900

    feat: Tuist Firebase Dependency 생성

--- Tuist/ProjectDescriptionHelpers/TargetDependency+extensions.swift
+++ Tuist/ProjectDescriptionHelpers/TargetDependency+extensions.swift
@@ -57,4 +57,6 @@ public enum ExternalDependency: String {
   case nukeUI = "NukeUI"
   case lottie = "Lottie"
   case kakaoSDK = "KakaoSDK"
+  case firebaseAnalytics = "FirebaseAnalytics"
+  case firebaseMessaging = "FirebaseMessaging"
 }
