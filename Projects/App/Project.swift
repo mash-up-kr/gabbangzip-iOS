5ee023112ff0ed9febffc62021ab039e3e6b2d18
YangJoonHyeok <zzing1031@gmail.com>
2024-07-28 20:56:22 +0900

    chore: Other linker flags 추가 위치 변경

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -25,8 +25,7 @@ let project = Project.make(
           "DEVELOPMENT_TEAM": "MYR6MP3VKX",
           "CODE_SIGN_STYLE": "Manual",
           "PROVISIONING_PROFILE_SPECIFIER": "match Development com.mashup.gabbangzip",
-          "CODE_SIGN_IDENTITY": "Apple Development: Hyerin Choe (QKKN56KGD9)",
-          "OTHER_LDFLAGS": "-ObjC"
+          "CODE_SIGN_IDENTITY": "Apple Development: Hyerin Choe (QKKN56KGD9)"
         ],
         configurations: [
           .release(name: .release, xcconfig: "./xcconfigs/Gabbangzip.release.xcconfig")
@@ -54,8 +53,7 @@ let project = Project.make(
           "DEVELOPMENT_TEAM": "MYR6MP3VKX",
           "CODE_SIGN_STYLE": "Manual",
           "PROVISIONING_PROFILE_SPECIFIER": "match Development com.mashup.gabbangzip-dev",
-          "CODE_SIGN_IDENTITY": "Apple Development: Hyerin Choe (QKKN56KGD9)",
-          "OTHER_LDFLAGS": "-ObjC"
+          "CODE_SIGN_IDENTITY": "Apple Development: Hyerin Choe (QKKN56KGD9)"
         ],
         configurations: [
           .debug(name: .debug, xcconfig: "./xcconfigs/Gabbangzip.debug.xcconfig")
