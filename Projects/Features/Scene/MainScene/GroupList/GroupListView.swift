e52fa9fc207759d4112e8c0b306b2a7997a90506
YangJoonHyeok <zzing1031@gmail.com>
2024-08-02 00:23:11 +0900

    refactor: SmallButton 수정

--- Projects/Features/Scene/MainScene/GroupList/GroupListView.swift
+++ Projects/Features/Scene/MainScene/GroupList/GroupListView.swift
@@ -105,14 +105,14 @@ extension GroupListView {
             photoCardFrontView(from: group)
             
             if group.status == .noPastAndCurrentEvent {
-              SmallButton(type: .constant(.active), smallButtonContentType: .generateEvent, action: {})
+              SmallButton(type: .active, smallButtonContentType: .generateEvent, action: {})
             }
           }
         }
         .padding(.horizontal, 41.5)
         
         if let buttonType = mapButtonType(from: group.status) {
-          SmallButton(type: .constant(.active), smallButtonContentType: buttonType, action: {})
+          SmallButton(type: .active, smallButtonContentType: buttonType, action: {})
         }
       }
     }
