60442ebab811b64ebd193119ce09e3ed1399412d
hyerin <hope1053@naver.com>
2024-08-02 20:43:56 +0900

    feat: GroupDetail API 구현
    # Conflicts:
    #	Projects/Core/Services/PicAPIClients/GroupAPI/GroupAPI.swift
    #	Projects/Core/Services/PicAPIClients/GroupAPI/GroupAPIClient.swift

--- Projects/Core/Services/PicAPIClients/GroupAPI/GroupAPIClient.swift
+++ Projects/Core/Services/PicAPIClients/GroupAPI/GroupAPIClient.swift
@@ -14,6 +14,7 @@ import Models
 @DependencyClient
 public struct GroupAPIClient: Sendable {
   public var getGroups: @Sendable (_ accessToken: String) async throws -> GroupsData
+  public var getGroupDetail: @Sendable (_ accessToken: String, _ groupID: Int) async throws -> GroupDetailInfo
   public var joinGroup: @Sendable (_ accessToken: String, _ code: String) async throws -> GroupID
   public var getMemberList: @Sendable (_ accessToken: String, _ groupID: Int) async throws -> MemberList
 }
@@ -58,6 +59,18 @@ extension GroupAPIClient: DependencyKey {
             code: .failToGetMemberList,
             underlying: error
           )
+        },
+      getGroupDetail: { accessToken, groupID in
+        let route = GroupAPI.getGroupDetail(accessToken: accessToken, groupID: groupID)
+        let request = Request<SuccessResponse<GroupDetailInfo>>(route: route)
+        do {
+          let response = try await NetworkManager.shared.send(request)
+          return response.value.data
+        } catch {
+          throw GroupAPIClientError(
+            code: .failToGetGroupDetail,
+            underlying: error
+          )
         }
       }
     )
@@ -67,12 +80,15 @@ extension GroupAPIClient: DependencyKey {
     return GroupAPIClient(
       getGroups: { _ in
         return GroupsData.mock
-      }, 
+      },
       joinGroup: { _, _ in
         return GroupID.mock
       },
       getMemberList: { _, _ in
         return MemberList.mock
+      },
+      getGroupDetail: { _, _ in
+        return GroupDetailInfo.mock
       }
     )
   }
@@ -93,6 +109,7 @@ public struct GroupAPIClientError: GabbangzipError {
 
   public enum APIResponseError: Int {
     case failToGetGroups
+    case failToGetGroupDetail
     case failToJoinGroup
     case failToGetMemberList
   }
