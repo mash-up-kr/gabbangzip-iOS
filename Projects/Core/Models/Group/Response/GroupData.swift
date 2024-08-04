91456bd63c99d396e236aee45b7d743c9c4d446b
YangJoonHyeok <zzing1031@gmail.com>
2024-08-05 00:47:21 +0900

    fix: 변경된 API Response 반영

--- Projects/Core/Models/Group/Response/GroupData.swift
+++ Projects/Core/Models/Group/Response/GroupData.swift
@@ -9,21 +9,27 @@
 import Foundation
 
 public struct GroupData: Decodable, Hashable {
+  public let id: Int
   public let name: String
   public let keyword: Keyword
   public let status: Status
   public let statusDescription: String
-  public let recentEventDate: String
+  public let recentEvent: RecentEvent
   public let cardFrontImageURL: String
-  public let cardBackImages: [CardBackImage]
+  public let cardBackImages: [CardBackImage]?
   
   enum CodingKeys: String, CodingKey {
-    case name, keyword, status
+    case id, name, keyword, status
     case statusDescription = "status_description"
-    case recentEventDate = "recent_event_date"
+    case recentEvent = "recent_event"
     case cardFrontImageURL = "card_front_image_url"
     case cardBackImages = "card_back_images"
   }
+}
+
+public struct RecentEvent: Decodable, Hashable {
+  public let name: String?
+  public let date: String?
 }
 
 extension GroupData {
