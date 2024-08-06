d55ed9cbc0d6336974b74b2231b2d5a9ac1069f8
hyerin <hope1053@naver.com>
2024-08-06 23:28:10 +0900

    correct: mock data 변경

--- Projects/Features/Scene/GroupDetailScene/GroupDetail/GroupDetailView.swift
+++ Projects/Features/Scene/GroupDetailScene/GroupDetail/GroupDetailView.swift
@@ -56,26 +56,12 @@ public struct GroupDetailView: View {
   }
 }
 
-// 이벤트 목록 빈 경우
 #Preview {
   GroupDetailView(
     store: Store(
       initialState: .init(
         groupID: 0,
         groupDetail: .noHistorymock
-      ),
-      reducer: GroupDetailCore.init
-    )
-  )
-}
-
-// 이벤트 목록 있는 경우
-#Preview {
-  GroupDetailView(
-    store: Store(
-      initialState: .init(
-        groupID: 0,
-        groupDetail: .historymock
       ),
       reducer: GroupDetailCore.init
     )
