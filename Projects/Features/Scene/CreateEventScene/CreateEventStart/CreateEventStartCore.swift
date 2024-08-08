4999efb76659e6a6b3eb362035f1fecf5dfa06c0
Serena <sha0720@naver.com>
2024-08-09 01:03:36 +0900

    feat: CreateEventStartView, CreateEventStartCore 셍성

--- Projects/Features/Scene/CreateEventScene/CreateEventStart/CreateEventStartCore.swift
+++ Projects/Features/Scene/CreateEventScene/CreateEventStart/CreateEventStartCore.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
 //
 
-import Foundation
+import ComposableArchitecture
+
+@Reducer
+public struct CreateEventStartCore {
+  public init() {}
+  
+  @ObservableState
+  public struct State: Equatable {
+    public var groupName: String
+    
+    public init(
+      groupName: String
+    ) {
+      self.groupName = groupName
+    }
+  }
+
+  public enum Action {
+    // View Action
+    case onAppear
+    case createEventButtonTapped
+    
+    // Route Action
+    case moveToGroupDetail
+    case moveToGroupMemberList
+    case moveToCreateEventProcess
+  }
+
+  public var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .onAppear:
+        return .none
+        
+      case .createEventButtonTapped:
+        return .send(.moveToCreateEventProcess)
+        
+      case .moveToGroupDetail:
+        return .none
+        
+      case .moveToGroupMemberList:
+        return .none
+        
+      case .moveToCreateEventProcess:
+        return .none
+      }
+    }
+  }
+}
