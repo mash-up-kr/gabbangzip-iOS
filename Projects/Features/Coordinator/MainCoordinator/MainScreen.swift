eeb97cac35b318edf74c76ebb776b0523ca38583
Serena <sha0720@naver.com>
2024-08-09 00:06:45 +0900

    feat: CreateEvent, CreateEventCoordinator

--- Projects/Features/Coordinator/MainCoordinator/MainScreen.swift
+++ Projects/Features/Coordinator/MainCoordinator/MainScreen.swift
@@ -7,6 +7,7 @@
 //
 
 import ComposableArchitecture
+import CreateEventCoordinator
 import CreateGroupCoordinator
 import GroupDetailCoordinator
 import Main
---
+++ Projects/Features/Scene/CreateEventScene/CreateEventCompletion/CreateEventCompletionView.swift
@@ -0,0 +1,9 @@
+//
+//  CreateEventCompletionView.swift
+//  CreateEvent
+//
+//  Created by Hyun A Song on 8/8/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+import Foundation
