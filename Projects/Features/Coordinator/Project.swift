5eea4e2bd97b85f368ed67ba6c8d0566a23de7b9
Serena <sha0720@naver.com>
2024-08-08 23:28:34 +0900

    feat: CreateEventCoordinator, CreateEvent tuist Dependency 생성

--- Projects/Features/Coordinator/Project.swift
+++ Projects/Features/Coordinator/Project.swift
@@ -23,6 +23,7 @@ let project = Project.make(
         .project(target: .main, projectPath: .scene),
         .project(target: .login, projectPath: .scene),
         .target(name: .createGroupCoordinator),
+        .target(name: .createEventCoordinator),
         .target(name: .myPageCoordinator),
         .external(externalDependency: .composableArchitecture),
         .external(externalDependency: .tcaCoordinators),
@@ -35,6 +36,17 @@ let project = Project.make(
       sources: ["CreateGroupCoordinator/**"],
       dependencies: [
         .project(target: .createGroup, projectPath: .scene),
+        .external(externalDependency: .composableArchitecture),
+        .external(externalDependency: .tcaCoordinators),
+      ]
+    ),
+    .make(
+      name: "CreateEventCoordinator",
+      product: .staticLibrary,
+      bundleId: "com.mashup.gabbangzip.createEventCoordinator",
+      sources: ["CreateEventCoordinator/**"],
+      dependencies: [
+        .project(target: .creatEvent, projectPath: .scene),
         .external(externalDependency: .composableArchitecture),
         .external(externalDependency: .tcaCoordinators),
       ]
