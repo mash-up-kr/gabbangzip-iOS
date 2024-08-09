91d2c315a164ed11ae0510106ba18fae100b6543
Serena <sha0720@naver.com>
2024-08-09 13:52:37 +0900

    feat: CreateEventProcessCore, CreateEventProcessView

--- Projects/Features/Scene/CreateEventScene/CreateEventCompletion/CreateEventCompletionCore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CreateEventCompletionCore.swift
-//  CreateEvent
-//
-//  Created by Hyun A Song on 8/8/24.
-//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
-//
-
-import Foundation
