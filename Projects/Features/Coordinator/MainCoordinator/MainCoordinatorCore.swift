a57fd8c1c1b874660f5c58533dd12fb9bf26ab1c
Serena <sha0720@naver.com>
2024-08-09 15:41:39 +0900

    feat: GroupList에서 이벤트 생성하기 클릭시 CreateEventCoordinator 연결

--- Projects/Features/Coordinator/MainCoordinator/MainCoordinatorCore.swift
+++ Projects/Features/Coordinator/MainCoordinator/MainCoordinatorCore.swift
@@ -41,7 +41,10 @@ public struct MainCoordinatorCore {
       case .router(.routeAction(id: _, action: .home(.moveToJoinGroup))):
         state.routes.push(.joinGroup(.init()))
         
-      case .router(.routeAction(id: _, action: .joinGroup(.backToHome))):
+      case .router(.routeAction(id: _, action: .groupList(.moveToEvent))):
+        state.routes.presentCover(.createEventCoordinator(.init(routes: [.root(.createEventStart(.init()), embedInNavigationView: true)])))
+        
+      case .router(.routeAction(id: _, action: .joinGroup(.backToGroupList))):
         state.routes.pop()
         
       case .router(.routeAction(id: _, action: .createGroupCoordinator(.router(.routeAction(id: _, action: .createGroupCompletion(.backToHome)))))):
