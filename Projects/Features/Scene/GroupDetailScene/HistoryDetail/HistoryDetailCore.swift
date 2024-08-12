0a7699cd88e2bbadc3e11d5f1969b3dc93f0d32b
hyerin <hope1053@naver.com>
2024-08-13 01:07:34 +0900

    feat: HistoryDetailView 구현
    # Conflicts:
    #	Projects/Features/Scene/GroupDetailScene/HistoryDetail/HistoryDetailCore.swift

--- Projects/Features/Scene/GroupDetailScene/HistoryDetail/HistoryDetailCore.swift
+++ Projects/Features/Scene/GroupDetailScene/HistoryDetail/HistoryDetailCore.swift
@@ -14,20 +14,35 @@ public struct HistoryDetailCore {
   @ObservableState
   public struct State: Equatable {
     var history: History
+    var keyword: GroupData.Keyword
+    var s3BucketDomain: String
     
-    public init(history: History) {
+    public init(
+      history: History,
+      keyword: GroupData.Keyword,
+      s3BucketDomain: String
+    ) {
       self.history = history
+      self.keyword = keyword
+      self.s3BucketDomain = s3BucketDomain
     }
   }
 
   public enum Action {
+    // View Action
     case backButtonTapped
+    
+    // Route Action
+    case backToGroupDetail
   }
 
   public var body: some Reducer<State, Action> {
     Reduce { state, action in
       switch action {
       case .backButtonTapped:
+        return .none
+        
+      case .backToGroupDetail:
         return .none
       }
     }
