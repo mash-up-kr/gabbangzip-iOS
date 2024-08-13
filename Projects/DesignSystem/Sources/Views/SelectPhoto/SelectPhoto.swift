3a88caa6818627c4d7705d19d2c4fe53e442c92e
Serena <sha0720@naver.com>
2024-08-13 11:33:20 +0900

    refactor: SelectPhoto 불필요한 HStack 삭제

--- Projects/DesignSystem/Sources/Views/SelectPhoto/SelectPhoto.swift
+++ Projects/DesignSystem/Sources/Views/SelectPhoto/SelectPhoto.swift
@@ -21,32 +21,34 @@ public struct SelectPhoto: View {
   }
   
   public var body: some View {
-    HStack(spacing: 0) {
-      VStack(spacing: 0) {
-        DesignSystem.Icons.picture
-          .resizable()
-          .frame(width: 28, height: 28)
-          .padding(.bottom, 4)
-        
-        HStack(spacing: 0) {
-          Text("\(selectedPhotosInfo.count)")
-            .font(.body16)
-            .foregroundStyle(selectedPhotosInfo.isEmpty ? DesignSystem.Colors.gray60 : DesignSystem.Colors.gray80)
-          Text("/\(maxCount)")
-            .font(.body16)
-            .foregroundStyle(DesignSystem.Colors.gray60)
-        }
+    VStack(spacing: 0) {
+      DesignSystem.Icons.picture
+        .resizable()
+        .frame(width: 28, height: 28)
+        .padding(.bottom, 4)
+      
+      HStack(spacing: 0) {
+        Text("\(selectedPhotosInfo.count)")
+          .font(.body16)
+          .foregroundStyle(
+            selectedPhotosInfo.isEmpty
+            ? DesignSystem.Colors.gray60
+            : DesignSystem.Colors.gray80
+          )
+        Text("/\(maxCount)")
+          .font(.body16)
+          .foregroundStyle(DesignSystem.Colors.gray60)
       }
-      .frame(width: 100, height: 100)
-      .background(DesignSystem.Colors.gray40)
-      .cornerRadius(10)
-      .overlay{
-        RoundedRectangle(cornerRadius: 10)
-          .stroke(DesignSystem.Colors.gray50, lineWidth: 1)
-      }
-      .padding(.leading, 16)
-      .padding(.top, 8)
     }
+    .frame(width: 100, height: 100)
+    .background(DesignSystem.Colors.gray40)
+    .cornerRadius(10)
+    .overlay{
+      RoundedRectangle(cornerRadius: 10)
+        .stroke(DesignSystem.Colors.gray50, lineWidth: 1)
+    }
+    .padding(.leading, 16)
+    .padding(.top, 8)
   }
 }
 
