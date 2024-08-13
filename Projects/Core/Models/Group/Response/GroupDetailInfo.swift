53c5ee54d7c916ddac6b548e514858f1ab3e7cfe
hyerin <hope1053@naver.com>
2024-08-13 16:56:25 +0900

    chore: mock data 수정

--- Projects/Core/Models/Group/Response/GroupDetailInfo.swift
+++ Projects/Core/Models/Group/Response/GroupDetailInfo.swift
@@ -109,43 +109,47 @@ public struct History: Decodable, Identifiable, Equatable {
       id: 0,
       name: "모임 이름1",
       date: "2024.06.01",
-      images: [.mock]
+      images: [
+        .init(
+          imageURL: "https://picsum.photos/200",
+          frame: .snowman
+        ),
+        .init(
+          imageURL: "https://picsum.photos/201",
+          frame: .snowman
+        ),
+        .init(
+          imageURL: "https://picsum.photos/202",
+          frame: .snowman
+        ),
+        .init(
+          imageURL: "https://picsum.photos/203",
+          frame: .snowman
+        )
+      ]
     ),
     .init(
       id: 1,
-      name: "모임 이름1",
+      name: "모임 이름2",
       date: "2024.06.01",
-      images: [.mock]
-    ),
-    .init(
-      id: 2,
-      name: "모임 이름1",
-      date: "2024.06.01",
-      images: [.mock]
-    ),
-    .init(
-      id: 3,
-      name: "모임 이름1",
-      date: "2024.06.01",
-      images: [.mock]
-    ),
-    .init(
-      id: 4,
-      name: "모임 이름1",
-      date: "2024.06.01",
-      images: [.mock]
-    ),
-    .init(
-      id: 5,
-      name: "모임 이름1",
-      date: "2024.06.01",
-      images: [.mock]
-    ),
-    .init(
-      id: 6,
-      name: "모임 이름1",
-      date: "2024.06.01",
-      images: [.mock]
+      images: [
+        .init(
+          imageURL: "https://picsum.photos/200",
+          frame: .snowman
+        ),
+        .init(
+          imageURL: "https://picsum.photos/201",
+          frame: .snowman
+        ),
+        .init(
+          imageURL: "https://picsum.photos/202",
+          frame: .snowman
+        ),
+        .init(
+          imageURL: "https://picsum.photos/203",
+          frame: .snowman
+        )
+      ]
     )
   ]
 }
