e5d3dc31088282ae10c1fc480aa395ef7bbb2a33
Serena <sha0720@naver.com>
2024-08-15 09:19:42 +0900

    feat: CreateEventInfo 생성

---
+++ Projects/Core/Models/Event/Response/CreateEventsInfo.swift
@@ -0,0 +1,33 @@
+//
+//  CreateEventsInfo.swift
+//  Models
+//
+//  Created by Hyun A Song on 8/15/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+public struct CreateEventsInfo: Decodable, Equatable {
+  public let groupID: Int
+  public let description: String
+  public let date: String
+  public let pictures: [String]
+  
+  public init(
+    groupID: Int,
+    description: String,
+    date: String,
+    pictures: [String]
+  ) {
+    self.groupID = groupID
+    self.description = description
+    self.date = date
+    self.pictures = pictures
+  }
+  
+  enum CodingKeys: String, CodingKey {
+    case groupID = "group_id"
+    case description
+    case date
+    case pictures
+  }
+}
