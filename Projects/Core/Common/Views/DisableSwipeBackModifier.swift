a22b2c34739835e9cf0c57213b1072a068496891
YangJoonHyeok <zzing1031@gmail.com>
2024-08-15 14:14:21 +0900

    feat: 홈 화면에 뒤로가기 스와이프 액션 비활성화 적용

---
+++ Projects/Core/Common/Views/DisableSwipeBackModifier.swift
@@ -0,0 +1,38 @@
+//
+//  DisableSwipeBackModifier.swift
+//  Common
+//
+//  Created by YangJoonHyeok on 8/15/24.
+//  Copyright © 2024 com.mashup.gabbangzip. All rights reserved.
+//
+
+import SwiftUI
+
+public struct DisableSwipeBackModifier: ViewModifier {
+  @State var userInteractionDisabled: Bool
+  
+  public init(userInteractionDisabled: Bool = false) {
+    self.userInteractionDisabled = userInteractionDisabled
+  }
+  
+  public func body(content: Content) -> some View {
+    content
+      .simultaneousGesture(
+        DragGesture()
+          .onChanged { value in
+            let isLeftToRightSwipe = value.translation.width > 0
+            if isLeftToRightSwipe {
+              userInteractionDisabled = true
+            }
+          }
+          .onEnded { value in
+            userInteractionDisabled = false
+          }
+      )
+      .overlay {
+        if userInteractionDisabled {
+          Color.clear
+        }
+      }
+  }
+}
